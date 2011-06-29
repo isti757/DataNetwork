587e312d632c3ee57dc61a15d0830f497f137d32
Igor <isti@isti757.(none)>
2011-06-29 17:07:44 +0200

    Some changes

---
+++ packet.h
@@ -0,0 +1,22 @@
+/*
+ * packet.h
+ *
+ *  Created on: Jun 28, 2011
+ *      Author: isti
+ */
+
+#ifndef PACKET_H_
+#define PACKET_H_
+
+#include <cnet.h>
+
+typedef struct
+{
+	int len;
+	char data[MAX_MESSAGE_SIZE];
+} PACKET;
+
+#define PACKET_HEADER_SIZE  sizeof(int)
+#define PACKET_SIZE(p)      (PACKET_HEADER_SIZE + p.len)
+
+#endif /* PACKET_H_ */
