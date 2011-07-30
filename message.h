ca0436fe7aae5b37dc1448735debd92cb72c2508
Igor <isti@isti757.(none)>
2011-07-30 14:30:53 +0200

    Message

---
+++ message.h
@@ -0,0 +1,17 @@
+/*
+ * message.h
+ *
+ *  Created on: Jul 29, 2011
+ *      Author: isti
+ */
+
+#ifndef MESSAGE_H_
+#define MESSAGE_H_
+
+typedef struct
+{
+	char data[MAX_MESSAGE_SIZE];
+
+} MSG;
+
+#endif /* MESSAGE_H_ */
