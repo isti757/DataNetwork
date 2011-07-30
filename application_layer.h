0383318d44fbdd33b0f9c44f84fb38e5a4056849
Igor <isti@isti757.(none)>
2011-07-30 15:57:09 +0200

    Application layer

--- application_layer.h
+++ application_layer.h
@@ -8,35 +8,16 @@
 #ifndef APPLICATION_LAYER_H_
 #define APPLICATION_LAYER_H_
 
-#include "frame.h"
 #include "packet.h"
 #include "transport_layer.h"
 
 //-----------------------------------------------------------------------------
-typedef struct application_layer_t {
-
-
-} application_layer_t;
+//typedef struct application_layer_t
+//{
+//	;
+//} application_layer_t;
 //-----------------------------------------------------------------------------
-application_layer_t application_layer;
-//-----------------------------------------------------------------------------
-static EVENT_HANDLER(application_ready) {
-	// read the message from application layer
-	CnetAddr destaddr;
-
-	MSG msg;
-	int msg_size = sizeof(MSG);
-	CHECK(CNET_read_application(&destaddr, (char *)msg, &msg_size));
-
-	// pass message to transport
-	PACKET pkt;
-	pkt.data = msg;
-	pkt.len = msg_size;
-
-	read_transport(destaddr,pkt);
-}
-//-----------------------------------------------------------------------------
-
+//application_layer_t application_layer;
 //-----------------------------------------------------------------------------
 
 #endif /* APPLICATION_LAYER_H_ */
