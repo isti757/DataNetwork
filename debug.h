c9ee9cb7a70c1b8918b1fe93eb2368e21bd71433
Igor <isti@isti757.(none)>
2011-07-30 15:57:55 +0200

    debugging

--- debug.h
+++ debug.h
@@ -15,26 +15,26 @@
 
 #define SHORT_DEBUG
 
-static void DEBUG1(char *description)
-{
-	#if defined(SHORT_DEBUG)
-		printf("%c\n\n", *description);
-	#endif
-}
-
-static void DEBUG2(char *description, int seq)
-{
-	#if defined(SHORT_DEBUG)
-		printf("%c sequence no. : %d\n\n", *description, seq);
-	#endif
-}
-
-static void DEBUG_NODE_STATISTICS()
-{
-#if defined(SHORT_DEBUG)
-	printf("Node statistics: %s\n", nodeinfo.nodename);
-	printf("message rate: %d\n\n", nodeinfo.messagerate);
-#endif
-}
+//static void DEBUG1(char *description)
+//{
+//	#if defined(SHORT_DEBUG)
+//		printf("%c\n\n", *description);
+//	#endif
+//}
+//
+//static void DEBUG2(char *description, int seq)
+//{
+//	#if defined(SHORT_DEBUG)
+//		printf("%c sequence no. : %d\n\n", *description, seq);
+//	#endif
+//}
+//
+//static void DEBUG_NODE_STATISTICS()
+//{
+//#if defined(SHORT_DEBUG)
+//	printf("Node statistics: %s\n", nodeinfo.nodename);
+//	printf("message rate: %d\n\n", nodeinfo.messagerate);
+//#endif
+//}
 
 #endif // APPLICATION_LAYER_H_
